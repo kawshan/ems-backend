ff1f2a9986ddb7248260ec40eb9aa76121e6838f
kawshan <kawshan.virantha9999@gmail.com>
2025-03-05 08:11:26 +0530

    react spring boot ems until update

---
+++ src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -0,0 +1,60 @@
+package net.javaguides.ems.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+
+
+    //build add employee rest api
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee =  employeeService.createEmployee(employeeDto);
+        return  new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    //build get employee rest api
+    @GetMapping(value = "{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+
+    //built get all employee
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+
+    //build update rest api
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto>updateEmployee(@PathVariable("id") Long employeeId, @RequestBody EmployeeDto updatedEmployee){
+        EmployeeDto employeeDto = employeeService.updateEmployee(employeeId,updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+
+
+
+
+
+
+
+
+
+}
